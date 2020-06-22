0f182d40190a99d4de35e50b353ae325fd993375
hynj <thenick112@gmail.com>
2020-06-22 17:12:43 +0100

    tailwind added 2

---
+++ src/renderer/components/MainPage.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <div class="w-full max-w-xs text-center">
+        <h3> Test Main page </h3><br>
+        <h2> Second Header</h2>
+         <p class="text-center text-gray-500 text-xs"></p>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainPage',
+    components:{},
+    methods:{}
+}
+</script>
